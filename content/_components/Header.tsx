6e61e03f3bd414b7c81862a68a90210294feee64
Torge Jürgensen <torge.juergensen@codeanker.de>
2023-10-31 21:38:21 +0100

    switch rakede animation to canvas

--- content/_components/Header.tsx
+++ content/_components/Header.tsx
@@ -3,5 +3,6 @@ export default ({ text, ...props }) => (
   <section class="bg-header">
     <img src="/assets/logo.webp" class="mx-auto drop-shadow-2xl relative z-10" id="logo" alt="hacks on the harbour logo"></img>
     <img src="/assets/header-back.svg" class="absolute w-full -z-10" alt=""></img>
+    <canvas id="rakede-canvas" class="fixed top-0 w-full h-full"></canvas>
   </section>
 );
--- content/assets/rakede-animation.js
+++ content/assets/rakede-animation.js
@@ -1,82 +1,147 @@
-const targetSelector = 'body';
 const rakedeImagePath = '/assets/rakede-farbe.webp';
-const rakedeSpeedPxPerSecond = 150;
-const rakedeSpeedVariation = 50;
+const rakedeAcceleration = 0.01;
 const countRakedes = 10;
 
-console.log('rakede-animation.js loading');
-document.addEventListener('DOMContentLoaded', function () {
+let mousePositionSet = false;
+let mouseX = 0;
+let mouseY = 0;
 
-  for (let i = 0; i < countRakedes; i++) {
-    // spawn with a delay to avoid all rakedes starting at the same time
-    setTimeout(() => {
-      spawnRakede();
-    }, i* 100 + Math.random() * 100);
+document.addEventListener('mousemove', function (event) {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+  mousePositionSet = true;
+})
+
+console.log('rakede-animation.js loading');
+document.addEventListener('DOMContentLoaded', async function () {
+  const canvas = document.getElementById('rakede-canvas');
+  resizeCanvas();
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
   }
+  window.addEventListener('resize', resizeCanvas);
+
+  const ctx = canvas.getContext('2d');
+
+  const rakedes = [];
+  for (let i = 0; i < countRakedes; i++) {
+    rakedes.push(await spawnRakede(ctx));
+
+    //
+  }
+  rakedes.forEach((rakede, index) => rakede.init({
+    delay: index * 1000 + Math.random() * 100
+  }))
+
+  function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    rakedes.forEach(rakede => {
+      rakede.update();
+      rakede.render(ctx);
+    });
+    requestAnimationFrame(update);
+  }
+  update();
 
   console.log('rakede-animation.js loaded');
 })
 
-function spawnRakede() {
-  const target = document.querySelector(targetSelector);
 
-  const image = document.createElement('img');
+async function spawnRakede(ctx) {
+  const image = new Image();
   image.src = rakedeImagePath;
-  image.style.position = 'fixed';
-  image.style.width = '25px';
-  image.style.height = '50px';
-
-  target.appendChild(image);
-  startRandomAnimation(image);
-}
 
 
-function startRandomAnimation(image) {
-  const screenHeight = window.innerHeight;
-  const screenWidth = window.innerWidth;
 
-  // get screen diagonal
-  const screenDiagonal = Math.sqrt(Math.pow(screenHeight, 2) + Math.pow(screenWidth, 2)) + 100;
-  const centerOfScreenX = screenWidth / 2;
-  const centerOfScreenY = screenHeight / 2;
-
-  const randomStartAngle = Math.random() * 2 * Math.PI;
-
-  const startX = Math.floor(centerOfScreenX + Math.cos(randomStartAngle) * screenDiagonal / 2);
-  const startY = Math.floor(centerOfScreenY + Math.sin(randomStartAngle) * screenDiagonal / 2);
-
-  const angleVariation = Math.PI / 3;
-  const randomTargetAngle = randomStartAngle + Math.PI + (Math.random() * 2 * angleVariation - angleVariation);
-
-  const targetX = Math.floor(centerOfScreenX + Math.cos(randomTargetAngle) * screenDiagonal / 2);
-  const targetY = Math.floor(centerOfScreenY + Math.sin(randomTargetAngle) * screenDiagonal / 2);
-
-  const angleStartToTarget = Math.atan2(targetY - startY, targetX - startX) * 180 / Math.PI + 90;
-  const distanceStartToTarget = Math.sqrt(Math.pow(targetY - startY, 2) + Math.pow(targetX - startX, 2));
-
-  const speed = rakedeSpeedPxPerSecond + Math.random() * 2 * rakedeSpeedVariation - rakedeSpeedVariation;
-  const duration = distanceStartToTarget / speed;
-
-  image.style.top = `${startY}px`;
-  image.style.left = `${startX}px`;
-  image.style.transition = `top ${duration}s ease-in, left ${duration}s ease-in`;
-  image.style.transform = `rotate(${angleStartToTarget}deg)`;
-
-  setTimeout(() => {
-    // move image to target position
-    image.style.top = `${targetY}px`;
-    image.style.left = `${targetX}px`;
-
-    // after the animation is finished, start a new random animation
-    setTimeout(() => {
-      // reset image to initial position
-      image.style.transition = '';
-      image.style.transform = '';
-      image.style.top = '';
-      image.style.left = '';
+  await new Promise(r => image.onload = r);
+  return {
+    rotation: 0,
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    maxSpeed: 10,
+    acceleration: rakedeAcceleration,
+    width: 25,
+    height: 50,
+    running: false,
+    init ({delay}) {
+      this.reset()
       setTimeout(() => {
-        startRandomAnimation(image)
-      }, 100)
-    }, duration * 1000);
-  }, 1);
+        this.running = true;
+      }, delay);
+    },
+    reset() {
+      const screenHeight = window.innerHeight;
+      const screenWidth = window.innerWidth;
+      const centerOfScreenX = screenWidth / 2;
+      const centerOfScreenY = screenHeight / 2;
+
+      const screenDiagonal = Math.sqrt(Math.pow(screenHeight, 2) + Math.pow(screenWidth, 2)) + 100;
+      const randomStartAngle = Math.random() * 2 * Math.PI;
+
+      const startX = Math.floor(centerOfScreenX + Math.cos(randomStartAngle) * screenDiagonal / 2);
+      const startY = Math.floor(centerOfScreenY + Math.sin(randomStartAngle) * screenDiagonal / 2);
+
+      const angleVariation = Math.PI / 3;
+      const randomTargetAngle = randomStartAngle + Math.PI + (Math.random() * 2 * angleVariation - angleVariation);
+
+      const targetX = Math.floor(centerOfScreenX + Math.cos(randomTargetAngle) * screenDiagonal / 2);
+      const targetY = Math.floor(centerOfScreenY + Math.sin(randomTargetAngle) * screenDiagonal / 2);
+
+      const angleStartToTarget = Math.atan2(targetY - startY, targetX - startX);
+
+      this.x = startX;
+      this.y = startY;
+      this.vx = 0;
+      this.vy = 0;
+      this.rotation = angleStartToTarget
+
+    },
+    render(xtx) {
+      if (!this.running) {
+        return;
+      }
+      xtx.save();
+      xtx.translate(this.x, this.y);
+      xtx.rotate(this.rotation + Math.PI/2);
+      xtx.drawImage(image, -this.width / 2, -this.height / 2, this.width, this.height);
+      xtx.restore();
+    },
+    update() {
+      if (!this.running) {
+        return;
+      }
+      this.vx += Math.cos(this.rotation) * this.acceleration;
+      this.vy += Math.sin(this.rotation) * this.acceleration;
+      this.x += this.vx;
+      this.y += this.vy;
+
+      const distanceToCenter = Math.sqrt(Math.pow(this.x - window.innerWidth / 2, 2) + Math.pow(this.y - window.innerHeight / 2, 2));
+      const screenDiagonal = Math.sqrt(Math.pow(window.innerHeight, 2) + Math.pow(window.innerWidth, 2));
+
+      if (mousePositionSet) {
+        // gravitational attract to mouse position
+        const distanceToMouse = Math.sqrt(Math.pow(this.x - mouseX, 2) + Math.pow(this.y - mouseY, 2));
+        const angleToMouse = Math.atan2(mouseY - this.y, mouseX - this.x);
+        const accelerationToMouse = Math.min(1 / Math.pow(distanceToMouse, 2), 1) * 100;
+        this.vx += Math.cos(angleToMouse) * accelerationToMouse;
+        this.vy += Math.sin(angleToMouse) * accelerationToMouse;
+      }
+
+      // limit speed
+      const speed = Math.sqrt(Math.pow(this.vx, 2) + Math.pow(this.vy, 2));
+      if (speed > this.maxSpeed) {
+        this.vx *= this.maxSpeed / speed;
+        this.vy *= this.maxSpeed / speed;
+      }
+
+      this.rotation = Math.atan2(this.vy, this.vx);
+
+      if (distanceToCenter > screenDiagonal / 2 + 100) {
+        this.reset();
+      }
+    }
+  }
 }
