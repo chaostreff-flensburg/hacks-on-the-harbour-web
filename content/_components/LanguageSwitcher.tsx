b8d6722c3dcf02d4d864810846e511848e3de7b9
Torge Jürgensen <torge.juergensen@codeanker.de>
2023-11-07 19:35:15 +0100

    responsive fix

--- content/_components/LanguageSwitcher.tsx
+++ content/_components/LanguageSwitcher.tsx
@@ -1,5 +1,5 @@
 export default (props) => (
-  <div class="bg-white flex w-fit text-xs md:text-xl py-2 px-4 mb-2 rounded space-x-4 underline shadow-lg">
+  <div class="bg-white flex w-fit text-xs md:text-md py-2 px-4 mb-2 rounded space-x-4 underline shadow-lg">
     {props.alternates?.map((alt) => (
       <a className={alt.lang === props.lang ? "font-bold" : ""} href={alt.url}>
         {alt.text.language}
